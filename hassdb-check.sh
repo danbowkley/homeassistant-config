12da887c6ae04e32f69075ca5e48a0f461357c18
Dan Bowkley <danbowkley@gmail.com>
2018-09-02 05:40:55 -0500

    Added Xerox color laser printer monitoring

--- hassdb-check.sh
+++ hassdb-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 service home-assistant@homeassistant stop
-mysqldump -u hassuser -p=Ep8nLKd9RJ3z6lRM --create-options --routines --triggers hass > ./db.dmp
-mysqlcheck -u hassuser -p=Ep8nLKd9RJ3z6lRM --check --databases hass
-mysqlcheck -u hassuser -p=Ep8nLKd9RJ3z6lRM --optimize --databases hass
+mysqldump -u hassuser -pEp8nLKd9RJ3z6lRM --create-options --routines --triggers hass > ./db.dmp
+mysqlcheck -u hassuser -pEp8nLKd9RJ3z6lRM --check --databases hass
+mysqlcheck -u hassuser -pEp8nLKd9RJ3z6lRM --optimize --databases hass
 service home-assistant@homeassistant start
